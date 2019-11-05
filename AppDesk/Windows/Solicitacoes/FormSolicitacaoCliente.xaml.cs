cb869a9e322d91d914231def813046b87601f298
Andre JS <andrej.silva@outlook.com>
2019-11-05 20:28:41 -0300

    Iniciado formulario de solicitação

---
+++ AppDesk/Windows/Solicitacoes/FormSolicitacaoCliente.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppDesk.Windows.Solicitacoes
+{
+    /// <summary>
+    /// Lógica interna para FormSolicitacaoCliente.xaml
+    /// </summary>
+    public partial class FormSolicitacaoCliente : Window
+    {
+        public FormSolicitacaoCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void AprovarBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReprovarBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoverBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
