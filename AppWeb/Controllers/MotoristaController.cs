229efdc6bc71a4183b91fef4413df73dcfe2fe99
Andre JS <andrej.silva@outlook.com>
2019-11-08 16:33:13 -0300

    Adicionada view de lista e detalhes de motoristas

--- AppWeb/Controllers/MotoristaController.cs
+++ AppWeb/Controllers/MotoristaController.cs
@@ -1,6 +1,13 @@
-﻿using System;
+﻿using AppWeb.Models.Motoristas;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Modelo.Classes.Web;
+using Persistencia.DAL.Usuarios.Web;
+using Servicos.Web;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +15,48 @@ namespace AppWeb.Controllers
 {
     public class MotoristaController : Controller
     {
+        private GerenciadorUsuarios Gerenciador
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<GerenciadorUsuarios>();
+            }
+        }
+
+        private MotoristaService MotoristaService = new MotoristaService();
+
         // GET: Motorista
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            string email = HttpContext.GetOwinContext().Authentication.User.Identity.Name;
+            long? id = (long?)long.Parse(Gerenciador.FindByEmail(email).Id);
+
+            List<Motorista> motoristas = MotoristaService.ObterMotoristasOrdPorId().Where(m => m.MotoristaId == id).ToList();
+
+            return View(motoristas);
+        }
+
+        [Authorize]
+        public ActionResult Details(long? id)
+        {
+            string email = HttpContext.GetOwinContext().Authentication.User.Identity.Name;
+            long? idCliente = (long?)long.Parse(Gerenciador.FindByEmail(email).Id);
+
+            Motorista motorista = MotoristaService.ObterMotoristaPorId(id);
+            if (motorista != null)
+            {
+                if (motorista.ClienteId != idCliente)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+                }
+
+                MotoristaViewModel motoristaView = new MotoristaViewModel() { Motorista = motorista };
+
+                return View(motoristaView);
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.NotFound);
         }
     }
 }
---
+++ AppWeb/Models/Motoristas/MotoristaViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Modelo.Classes.Desk;
+using Modelo.Classes.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppWeb.Models.Motoristas
+{
+    public class MotoristaViewModel
+    {
+        public Motorista Motorista { get; set; }
+
+        public IEnumerable<Multa> Multas 
+        { 
+            get
+            {
+                return Motorista.Multas;
+            }
+        }
+
+        public IEnumerable<Sinistro> Sinistros
+        {
+            get
+            {
+                return Motorista.Sinistros;
+            }
+        }
+    }
+}
