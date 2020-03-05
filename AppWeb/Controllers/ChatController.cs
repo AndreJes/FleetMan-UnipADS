696d6beffe0c4c19a4131aa1a9ed4f8d5e7858c4
Andre JS <andrej.silva@outlook.com>
2020-03-05 18:51:27 -0300

    Inserção da Partial View do chat bot no layout da pagina web

---
+++ AppWeb/Controllers/ChatController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace AppWeb.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult GetMessage()
+        {
+            return new JsonResult { Data = new {Message = "Hello JSON!", Number = 1 }, JsonRequestBehavior = JsonRequestBehavior.AllowGet, ContentType = "application/json" };
+        }
+    }
+}
